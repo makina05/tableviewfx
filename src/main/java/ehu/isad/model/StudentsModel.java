3535f8ced5e5be3801ca0afe728159ce56f2e7c9
Juanan Pereira <juanan@ikasten.io>
2019-11-11 16:36:13 +0100

    buildable

---
+++ src/main/java/ehu/isad/model/StudentsModel.java
@@ -0,0 +1,47 @@
+package ehu.isad.model;
+
+import javafx.scene.image.Image;
+
+public class StudentsModel {
+
+    private Integer studentId;
+    private String firstName;
+    private String lastName;
+    private Image image;
+    // private String imagePath;
+
+    public StudentsModel(Integer studentId, String firstName, String lastName, String imagePath) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.image = new Image(imagePath);
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {this.studentId = studentId;}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,7 @@ open module scenebuilder {
   requires javafx.controls;
   requires javafx.graphics;
   requires javafx.fxml;
+  requires flickr4java;
+  requires scribejava.core;
   exports ehu.isad;
 }

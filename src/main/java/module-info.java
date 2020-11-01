33c64d86b3a63bc656dadda221f8df38d29b2c5d
Juanan Pereira <juanan@ikasten.io>
2020-11-01 20:24:36 +0100

    fix build.gradle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,5 @@ open module scenebuilder {
   requires javafx.controls;
   requires javafx.graphics;
   requires javafx.fxml;
-  requires flickr4java;
-  requires scribejava.core;
   exports ehu.isad;
 }

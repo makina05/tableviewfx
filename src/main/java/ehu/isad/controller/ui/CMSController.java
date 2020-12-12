c790dc3fad1a9e1bc439b8092b0726a53afbb17c
34636 <ibaifranco@gmail.com>
2020-12-12 10:53:35 +0100

    Filtroa aldatuta, konpontzeke

--- src/main/java/ehu/isad/controller/ui/CMSController.java
+++ src/main/java/ehu/isad/controller/ui/CMSController.java
@@ -2,22 +2,31 @@ package ehu.isad.controller.ui;
 
 import java.net.URL;
 import java.util.*;
+import java.util.function.Predicate;
 
 import ehu.isad.db.WhatWebDBKud;
-import ehu.isad.model.Cms;
 import ehu.isad.model.Laguntzailea;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import javax.xml.transform.Result;
 
 
 public class CMSController {
     private ObservableList<Laguntzailea> data;
     private ObservableList<Laguntzailea> data2;
     private ObservableList<Laguntzailea> data3;
+    private Laguntzailea webgune;
+    private Laguntzailea laguntzailea;
 
 
     @FXML
@@ -83,22 +92,53 @@ public class CMSController {
         lastId.setCellValueFactory( new PropertyValueFactory<>("target_id"));
 
         tableViewId.setItems(data);
-
     }
 
     public void filtratu(){
-        String textua = fieldId.getText();
-        String izena = comboBoxId.getValue();
-        List<Laguntzailea> lista = WhatWebDBKud.getInstance().lortuOrrialdeak();
-        List<Laguntzailea> listaFiltratua=new ArrayList<>();
-        for (int i=0;i<lista.size();i++){
-            if (lista.get(i).getTarget().contains(textua) && lista.get(i).getString().contains(izena)){
-                listaFiltratua.add(lista.get(i));
-            }
-        }
-        data2 = FXCollections.observableArrayList(listaFiltratua);
-        tableViewId.setItems(data2);
+//        String textua = fieldId.getText();
+//        String izena = comboBoxId.getValue();
+//        List<Laguntzailea> lista = WhatWebDBKud.getInstance().lortuOrrialdeak();
+//        List<Laguntzailea> listaFiltratua=new ArrayList<>();
+//        for (int i=0;i<lista.size();i++){
+//            if (lista.get(i).getTarget().contains(textua) && lista.get(i).getString().contains(izena)){
+//                listaFiltratua.add(lista.get(i));
+//            }
+//        }
+//        data2 = FXCollections.observableArrayList(listaFiltratua);
+//        tableViewId.setItems(data2);
+
+
+
+
+        // see: https://stackoverflow.com/questions/42138867/filtering-jfx-tableview-with-multiple-values
+        List<Laguntzailea> lagak = WhatWebDBKud.getInstance().lortuOrrialdeak();
+        data2 = FXCollections.observableArrayList(lagak);
+
+        ObjectProperty<Predicate<Laguntzailea>> nameFilter = new SimpleObjectProperty<>();
+
+        nameFilter.bind(Bindings.createObjectBinding(() ->
+                        webgune ->  webgune.getTarget().toLowerCase().contains(fieldId.getText().toLowerCase()) ||
+                                webgune.getString().contains(fieldId.getText().toLowerCase()) ,
+                fieldId.textProperty()));
+
+        ObjectProperty<Predicate<Laguntzailea>> provinceFilter = new SimpleObjectProperty<>();
+        provinceFilter.bind(Bindings.createObjectBinding(() ->
+                        webgune -> comboBoxId.getValue().equals("") || comboBoxId.getValue().equals(  webgune.getString() ),
+                comboBoxId.valueProperty()));
+
+
+        FilteredList<Laguntzailea> filteredData = new FilteredList<>(data2, b->true);
+
+        filteredData.predicateProperty().bind(Bindings.createObjectBinding(
+                () -> nameFilter.get().and(provinceFilter.get()),
+                nameFilter, provinceFilter));
+
+//        SortedList<Laguntzailea> sortedData = new SortedList<>(filteredData);
+//        sortedData.comparatorProperty().bind(tableViewId.comparatorProperty());
+//        tableViewId.setItems(sortedData);
+        tableViewId.setItems(filteredData);
     }
+
 
 //    public void filtratuCombotik(){
 //        String izena = comboBoxId.getValue();
